866052f2e6ddbdfd59f9fc9553d37ec745f05354
AREEB <mohammadareeb34@gmail.com>
2024-06-02 01:28:02 +0800

    Add files via upload

---
+++ 1js.js
@@ -0,0 +1,25 @@
+
+document.querySelector("button").addEventListener("click",function(){
+    let newColor=randomColor();
+    
+    document.querySelector("div").style.backgroundColor=newColor;
+    document.querySelector("div").innerText="Here is the result";
+    document.querySelector("h1").innerText=newColor;
+    document.querySelector("h1").style.color=newColor;
+    document.querySelector("button").style.backgroundColor=newColor;
+   
+    });
+
+
+
+function randomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
+
+
+
+
